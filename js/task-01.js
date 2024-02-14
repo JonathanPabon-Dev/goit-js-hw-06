c5d7908bb9400cfd953a3aad3cfb1790801f8237
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-02-14 08:12:17 -0500

    Se ajusta la tarea 1 empleando foreach

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,11 @@
-// Seleccionar el elemento ul#categories 
-const categories = document.querySelector("#categories"); 
-// Obtener el número de categorías 
-const numberOfCategories = categories.children.length; 
-console.log(`Number of categories: ${numberOfCategories}`); 
-// Iterar sobre cada elemento li.item en la lista 
-for (const category of categories.children) 
-{ 
-    // Obtener el texto del título del artículo 
-    const categoryTitle = category.querySelector("h2").textContent; 
-    console.log(`Category: ${categoryTitle}`); 
-    // Obtener el número de artículos en la categoría 
-    const numberOfElements = category.querySelectorAll("li").length; 
-    console.log(`Elements: ${numberOfElements}`); 
-} 
+const categories = document.querySelector('#categories');
+const numberOfCategories = categories.children.length;
+console.log([...categories.children]);
+console.log(`Number of categories: ${numberOfCategories}`);
+
+[...categories.children].forEach(category => {
+  const categoryTitle = category.querySelector('h2').textContent;
+  console.log(`Category: ${categoryTitle}`);
+  const numberOfElements = category.querySelectorAll('li').length;
+  console.log(`Elements: ${numberOfElements}`);
+});
